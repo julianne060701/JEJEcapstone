fe3b61b3a0c2278b77de2f4c9a109a03aa918685
abys1 <varquez.220494@gensan.sti.edu.ph>
2023-04-14 02:42:30 +0900

    tete

--- lawyerregister.php
+++ lawyerregister.php
@@ -12,25 +12,6 @@ include "dbconn.php";
     $sql = "INSERT INTO tbl_userlawyer (firstName, lastName, userName, email, phone, password) VALUES
     ('$firstname', '$lastname', '$username', '$email', '$phone', '$password')";
 
-    // upload imaage
-    //   if (isset($_FILES['validid'])) {
-    //     $fileName = $_FILES['validid']['name'];
-    //     $fileType = $_FILES['validid']['type'];
-    //     $fileSize = $_FILES['validid']['size'];
-    //     $fileTemp = $_FILES['validid']['tmpName'];
-
-    //     if (($fileType == 'image/png' || $fileType == 'image/jpeg') && $fileSize > 0) {
-    //         //create unique name
-    //         $unique_file_name = time() . '_' . $fileName;
-    //         //set path
-    //         $filePath = 'images/' . $unique_file_name;
-    //         //move uploaded file to new location
-    //         move_uploaded_file($fileTemp, $filePath);
-    //         //insert to database
-    //         $sql = "INSERT INTO tbl_userlawyer (validID) VALUES ('$filePath')";
-
-    //     }
-    // }
   
 
     $result = mysqli_query($conn, $sql);
