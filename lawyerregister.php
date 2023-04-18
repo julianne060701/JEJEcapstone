350e9914a206a08a4ef0c4f9d8db7cb1a7f8857a
abys1 <varquez.220494@gensan.sti.edu.ph>
2023-04-18 20:58:24 +0900

    tete

--- lawyerregister.php
+++ lawyerregister.php
@@ -7,11 +7,10 @@ include "dbconn.php";
     $email = $_POST['email'];
     $phone = $_POST['phonenumber'];
     $password = $_POST['password_1'];
+    $usertype = $_POST['user_type'];
    
 
-    $sql = "INSERT INTO tbl_userlawyer (firstName, lastName, userName, email, phone, password) VALUES
-    ('$firstname', '$lastname', '$username', '$email', '$phone', '$password')";
-
+    
   
 
     $result = mysqli_query($conn, $sql);
