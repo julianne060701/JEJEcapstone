59f4433628a8d0ca4326374e49c5d67a0906f295
julianne060701 <fernandez.226952@gensan.sti.edu.ph>
2023-04-27 12:01:19 +0800

    hakdog mo

--- lawyersignup.php
+++ lawyersignup.php
@@ -27,29 +27,7 @@
 						<nav class="navbar navbar-expand-lg ">
 							<a class="navbar-brand cus-a" href="#">Lawyer Management System</a>
 							<div class="collapse navbar-collapse" id="navbarSupportedContent">
-								<ul class="navbar-nav ml-auto ">
-									<li class="active">
-										<a class="nav-link cus-a" href="index.php">Home <span class="sr-only">(current)</span></a>
-									</li>
-									<li class="">
-										<a class="nav-link cus-a" href="lawyers.php">Lawyers</a><!--lawyers.html page-->
-									</li>
-									<li class="">
-										<a class="nav-link cus-a" href="#">About Us</a>
-									</li>
-									<li class="">
-										<a class="nav-link cus-a" href="login.php">Login</a>
-									</li>
-									<li class="nav-item dropdown">
-										<a class="nav-link dropdown-toggle cus-a" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-											Register
-										</a>
-										<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-											<a class="dropdown-item" href="lawyer_register.php">Register as a lawyer</a><!--redirect lawyer register page-->
-											<a class="dropdown-item" href="user_register.php">Register as a user</a><!--redirect user register page-->
-
-										</div>
-									</li>
+								<ul class="navbar-nav ml-auto ">									
 
 
 								</ul>
@@ -307,11 +285,7 @@
 		</section>
 		<footer class ="bg-success">
 			<div class="container">
-				<div class="row">
-					<div class="col">
-						<h5>All rights reserved 2022</h5>
-					</div>
-				</div>
+			
 			</div>
 		</footer>
 
