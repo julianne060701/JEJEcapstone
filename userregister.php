e93141630db474961203e82fb40a37e0a63dcb6e
abys1 <varquez.220494@gensan.sti.edu.ph>
2023-05-11 03:48:28 +0900

    walanakokasabot

--- userregister.php
+++ userregister.php
@@ -1,16 +1,21 @@
 <?php
 include "dbconn.php";
 
-if (isset($_POST['firstname']) && isset($_POST['middlename']) && isset($_POST['lastname']) && isset($_POST['phonenumber'])
-&& isset($_POST['email']) && isset($_POST['password_1']) && isset($_POST['cfpassword'])) {
+      use PHPMailer\PHPMailer\PHPMailer;
+      use PHPMailer\PHPMailer\SMTP;
+      use PHPMailer\PHPMailer\Exception;
+      require 'vendor/autoload.php';
 
+   if (isset($_POST['firstname']) && isset($_POST['middlename']) && isset($_POST['lastname']) && isset($_POST['phonenumber'])
+   && isset($_POST['email']) && isset($_POST['password_1']) && isset($_POST['cfpassword'])) {
+      
         function validate($data) {
             $data = trim($data);
             $data = stripslashes($data);
             $data = htmlspecialchars($data);
             return $data;
         }
-
+       
         $firstname = validate($_POST['firstname']);
         $middlename = validate($_POST['middlename']);
         $lastname = validate($_POST['lastname']);
@@ -20,6 +25,27 @@ if (isset($_POST['firstname']) && isset($_POST['middlename']) && isset($_POST['l
         $encrypted_password = password_hash($password, PASSWORD_DEFAULT);
         $cfpassword = validate($_POST['cfpassword']);
         $regex = '/^[a-zA-Z ]*$/';
+        $mail = new PHPMailer(true);
+
+        $name = $firstname . $middlename . $lastname;
+
+        //OTP
+        $mail->SMTPDebug = 0;
+        $mail->isSMTP();   
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'legaleas3@gmail.com';
+        $mail->Password = 'jccfqxsnjttnhtwo';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+        $mail->setFrom('legaleas3@gmail.com', 'legalease.com');
+        $mail->addAddress($email, $name);
+        $mail->isHTML(true);
+        $otp = substr(number_format(time() * rand(), 0, '', ''), 0, 6);
+        $mail->Subject = 'OTP verification';
+        $mail->Body    = '<p>Your OTP code is: <b style="font-size: 30px;">' . $otp . '</b></p>';
+        $mail->send();
+  
 
       if (empty($firstname)) {
          header("Location: ClientRegister.php?error= First Name cannot be empty!");
@@ -57,7 +83,7 @@ if (isset($_POST['firstname']) && isset($_POST['middlename']) && isset($_POST['l
          header("Location: ClientRegister.php?error= Password and Confirm Password should be the same");
          exit();
       }else {
-         
+
             // Insert user information in tbl_userinfo table
             $sql = "INSERT INTO tbl_userinfo (firstName, middlename, lastName, phoneNum, address) VALUES ('$firstname','$middlename' ,'$lastname','$phonenumber', '$address')";
 
@@ -65,7 +91,7 @@ if (isset($_POST['firstname']) && isset($_POST['middlename']) && isset($_POST['l
                 $user_id = $conn->insert_id;
 
                 // Insert user credentials in tbl_cred table
-                $sql = "INSERT INTO tbl_cred (user_id, email, password, otp, email_verified) VALUES ('$user_id', '$email', '$encrypted_password', '$otp', NULL)";
+                $sql = "INSERT INTO tbl_cred (user_id, email, password, otp, email_verified_at) VALUES ('$user_id', '$email', '$encrypted_password', '$otp', NULL)";
                 if ($conn->query($sql) === TRUE) {
                     // Insert user type in tbl_usertype table
                     $sql = "INSERT INTO tbl_usertype (user_id, user_type) VALUES ('$user_id', 'client')";
