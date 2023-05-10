bfe6a003cb6eef9a0d3582788fdcb3cb2f29a205
julianne060701 <fernandez.226952@gensan.sti.edu.ph>
2023-05-11 04:21:01 +0800

    newbg home

--- homepage.php
+++ homepage.php
@@ -22,6 +22,7 @@
 </head>
 <body>
     <header>
+        
         <h2 class="logo"><img src="assets/imgs/law-firm-logo.jpg"alt=""> Law Office General Santos City</h2>
         <nav class="navigation">
     <a href="">Home</a>
@@ -38,7 +39,145 @@
 
     </header>
 
-    <div class="wrapper">
+
+    <!-- Description -->
+
+    <section class="slider">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="slider-content">
+                    <h2>Connecting You to Top Law Firms in General Santos City - Schedule Your Legal Consultation Today!</h2>
+                    <p>Welcome to our appointment service portal connecting you to the best law offices in General Santos City 
+                        - where you can easily schedule and book legal consultations for efficient and effective solutions to your legal needs. </p>
+                 
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="se-01">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="headding-01">
+                    <h2>Legal Practices Areas</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="box-se">
+                    <div class="icon-box-se">
+                        <i class="far fa-landmark"></i>
+                    </div>
+                    <h3>Civil Law</h3>
+                    <p>Civil law is a legal system based on written laws that define the rights and obligations of individuals and organizations in relation to each other.</p>
+                    <a href="#">Read More</a>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="box-se">
+                    <div class="icon-box-se">
+                        <i class="fas fa-users"></i>
+                    </div>
+                    <h3>Family Law</h3>
+                    <p>Family law is an area of the legal system that deals with matters related to family relationships, such as marriage, divorce, child custody, adoption, and domestic violence.</p>
+                    <a href="#">Read More</a>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="box-se">
+                    <div class="icon-box-se">
+                        <i class="fas fa-business-time"></i>
+                    </div>
+                    <h3>Business Law</h3>
+                    <p>Business law, also known as commercial law, is a legal area that deals with the rights, relations, and conduct of businesses and individuals engaged in commerce, including contract law, intellectual property law, and employment law.</p>
+                    <a href="#">Read More</a>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="box-se">
+                    <div class="icon-box-se">
+                        <i class="fab fa-battle-net"></i>
+                    </div>
+                    <h3>Cyber Law</h3>
+                    <p>Cyber law, also known as internet law, is a legal field that deals with the regulation of the internet, cyberspace, and technology, including electronic commerce, online privacy, data protection, and cybercrime.</p>
+                    <a href="#">Read More</a>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="box-se">
+                    <div class="icon-box-se">
+                        <i class="fas fa-gavel"></i>
+                    </div>
+                    <h3>Criminal Law</h3>
+                    <p>Criminal law is a legal system that deals with offenses committed against the public, including crimes such as murder, theft, assault, and fraud, and the procedures and penalties for prosecuting and punishing those who commit such crimes.</p>
+                    <a href="#">Read More</a>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="box-se">
+                    <div class="icon-box-se">
+                        <i class="fas fa-graduation-cap"></i>
+                    </div>
+                    <h3>Education Law</h3>
+                    <p>Education law is a legal area that governs the operation of educational institutions and the rights and responsibilities of students, parents, and teachers, including matters such as academic freedom, discrimination, school funding, special education, and student discipline.</p>
+                    <a href="#">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-6">
+                    <div class="_kl_de_w">
+                        <h3>Lawyer</h3>
+                        <p>ipsum dolor sit amet, Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-3 col-sm-6">
+                    <div class="_kl_de_w">
+                        <h3>Quick Links</h3>
+                        <ol>
+                            <li><i class="far fa-angle-right"></i>home</li>
+                            <li><i class="far fa-angle-right"></i>About Us</li>
+                            <li><i class="far fa-angle-right"></i>Services</li>
+                            <li><i class="far fa-angle-right"></i>Blog</li>
+                            <li class="last"><i class="far fa-angle-right"></i>Contact Us</li>
+                        </ol>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-3 col-sm-6">
+                    <div class="_kl_de_w">
+                        <h3>Our Legal</h3>
+                        <ol>
+                            <li><i class="far fa-angle-right"></i>Civil Law</li>
+                            <li><i class="far fa-angle-right"></i>Family Law</li>
+                            <li><i class="far fa-angle-right"></i>Business Law</li>
+                            <li><i class="far fa-angle-right"></i>Cyber Law</li>
+                            <li class="last"><i class="far fa-angle-right"></i>Criminal Law</li>
+                        </ol>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+<!-- popup login -->
+<div class="wrapper">
         <span class="icon-close"><ion-icon name="close"></ion-icon></span>
         <div class="form-box login">
             <h2>Login</h2>
@@ -67,6 +206,7 @@
             </form>
         </div>
 
+        <!-- Registration -->
         <div class="form-box register">
             <h2>Registration</h2>
             <form action="userregister.php" method="POST">
@@ -129,141 +269,68 @@
     </div>
 </body>
 
-<!-- <section class="se-01">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="headding-01">
-                    <h2>Our Legal Practices Areas</h2>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-6">
-                <div class="box-se">
-                    <div class="icon-box-se">
-                        <i class="far fa-landmark"></i>
-                    </div>
-                    <h3>Civil Law</h3>
-                    <p>Our attorneys are highly trained and skilled in the Civil Law sector to provide the top service.</p>
-                    <a href="#">Read More</a>
-                </div>
-            </div>
 
-            <div class="col-lg-4 col-md-4 col-sm-6">
-                <div class="box-se">
-                    <div class="icon-box-se">
-                        <i class="fas fa-users"></i>
-                    </div>
-                    <h3>Family Law</h3>
-                    <p>Our attorneys are highly trained and skilled in the Civil Law sector to provide the top service.</p>
-                    <a href="#">Read More</a>
-                </div>
-            </div>
 
-            <div class="col-lg-4 col-md-4 col-sm-6">
-                <div class="box-se">
-                    <div class="icon-box-se">
-                        <i class="fas fa-business-time"></i>
-                    </div>
-                    <h3>Business Law</h3>
-                    <p>Our attorneys are highly trained and skilled in the Civil Law sector to provide the top service.</p>
-                    <a href="#">Read More</a>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-4 col-sm-6">
-                <div class="box-se">
-                    <div class="icon-box-se">
-                        <i class="fab fa-battle-net"></i>
-                    </div>
-                    <h3>Cyber Law</h3>
-                    <p>Our attorneys are highly trained and skilled in the Civil Law sector to provide the top service.</p>
-                    <a href="#">Read More</a>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-4 col-sm-6">
-                <div class="box-se">
-                    <div class="icon-box-se">
-                        <i class="fas fa-gavel"></i>
-                    </div>
-                    <h3>Criminal Law</h3>
-                    <p>Our attorneys are highly trained and skilled in the Civil Law sector to provide the top service.</p>
-                    <a href="#">Read More</a>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-4 col-sm-6">
-                <div class="box-se">
-                    <div class="icon-box-se">
-                        <i class="fas fa-graduation-cap"></i>
-                    </div>
-                    <h3>Education Law</h3>
-                    <p>Our attorneys are highly trained and skilled in the Civil Law sector to provide the top service.</p>
-                    <a href="#">Read More</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 col-md-3 col-sm-6">
-                    <div class="_kl_de_w">
-                        <h3>Lawyer</h3>
-                        <p>ipsum dolor sit amet, Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-3 col-sm-6">
-                    <div class="_kl_de_w">
-                        <h3>Quick Links</h3>
-                        <ol>
-                            <li><i class="far fa-angle-right"></i>home</li>
-                            <li><i class="far fa-angle-right"></i>About Us</li>
-                            <li><i class="far fa-angle-right"></i>Services</li>
-                            <li><i class="far fa-angle-right"></i>Blog</li>
-                            <li class="last"><i class="far fa-angle-right"></i>Contact Us</li>
-                        </ol>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-3 col-sm-6">
-                    <div class="_kl_de_w">
-                        <h3>Our Legal</h3>
-                        <ol>
-                            <li><i class="far fa-angle-right"></i>Civil Law</li>
-                            <li><i class="far fa-angle-right"></i>Family Law</li>
-                            <li><i class="far fa-angle-right"></i>Business Law</li>
-                            <li><i class="far fa-angle-right"></i>Cyber Law</li>
-                            <li class="last"><i class="far fa-angle-right"></i>Criminal Law</li>
-                        </ol>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-3 col-sm-6">
-                    <div class="_kl_de_w">
-                        <h3>Contact Us</h3>
-                        <form class="my-form">
-                            <div class="form-group">
-                               <input class="form-control" type="emal" name="email" placeholder="Email"> 
-                            </div>
-                            <div class="form-group">
-                                <textarea rows="3" placeholder="Message" class="form-control"></textarea>
-                            </div>
-                            <div class="form-group">
-                                <a href="#">Submit</a>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer> -->
 <script src="assets/js/script.js"></script>
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+<script>
+  function openPopup() {
+    document.getElementById("login-popup").style.display = "block";
+  }
+
+  function closePopup() {
+    document.getElementById("login-popup").style.display = "none";
+  }
+
+  document.querySelector(".btnLogin-popup").addEventListener("click", openPopup);
+</script>
+
+</body>
+<script>
+    
+</script>
+<script>
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+</script>
+<script src="assets/js/jquery-3.2.1.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script>
+      $(document).ready (function(){
+        $('.menu-toggle').click(function(){
+          $('.menu-toggle').toggleClass('active')
+          $('.menu').toggleClass('active')
+        })
+      })
+</script>
+
+ <script>
+      $( () => {
+    
+        //On Scroll Functionality
+        $(window).scroll( () => {
+          var windowTop = $(window).scrollTop();
+          windowTop > 50 ? $('header').addClass('og-hf') : $('header').removeClass('og-hf');
+        });
+      });
+    </script>
 </html>
