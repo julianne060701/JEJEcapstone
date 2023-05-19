d7864999adaac895c44e9b28c53f0065e3bff0bc
julianne060701 <fernandez.226952@gensan.sti.edu.ph>
2023-05-19 19:46:06 +0800

    na ayus na ang login

--- homepage.php
+++ homepage.php
@@ -19,6 +19,7 @@
                 // include "css/homepage.css"
             // ?> -->
         <!-- </style> -->
+
 </head>
 <body>
     <header>
@@ -27,7 +28,8 @@
         <nav class="navigation">
     <a href="">Home</a>
     <a href="">About</a>
-    <Button class="btnLogin-popup" onclick="openPopup()">Login</Button>
+    <button id="openModal" class="btnmodal">Login</button>
+    <!-- <Button class="btnLogin-popup" onclick="openPopup()">Login</Button> -->
     <div class="dropdown">
         <button class="dropbtn btnLogin-popup">Register <span id="icon-ali"><ion-icon name="caret-down-outline"></ion-icon></span></button>
         <div class="dropdown-content">
@@ -38,6 +40,105 @@
 </nav>
 
     </header>
+
+    
+
+<div id="modal" class="modal">
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <div class="wrapper" id="login-popup">
+        <!-- <span class="icon-close"><ion-icon name="close"></ion-icon></span> -->
+        <div class="form-box login">
+            <h2>Login</h2>
+            <form action="userlogin.php" method="POST" >
+            <?php if (isset($_GET['error'])) { ?>
+			<p class="error"><?php echo $_GET['error']; ?> </p>
+		<?php } ?>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="email"></ion-icon></span>
+                    <input type="text" name="email">
+                    <label>Email</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="password"></ion-icon></span>
+                    <input type="password" name="password">  
+                    <label>password</label> 
+                </div>
+                <div class="remember-forgot">
+                    <label><input type="checkbox">Remember me</label>
+                    <a href="#">Forgot Password?</a>
+                </div>
+                <button type="submit" class="btn">Login</button>
+                <div class="login-register">
+                    <p><a href="#" class="register-link"></a></p>
+                </div>
+            </form>
+        </div>
+
+        <!-- Registration -->
+        <div class="form-box register">
+            <h2>Registration</h2>
+            <form action="userregister.php" method="POST">
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="mail"></ion-icon></span>
+                    <input type="text" name="firstname" >
+                    <label>First Name</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="mail"></ion-icon></span>
+                    <input type="text" name="lastname">
+                    <label>Last Name</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="at"></ion-icon></span>
+                    <input type="text" name="email">  
+                    <label>Email</label> 
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="navigate"></ion-icon></span>
+                    <input type="text" name="address">
+                    <label>Address</label>
+                </div>           
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="call"></ion-icon></span>
+                    <input type="text" name="phone">  
+                    <label>Phone Number</label> 
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                    <input type="password" name="password">
+                    <label>Password</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                    <input type="password" name="cfpassword">
+                    <label>Confirm Password</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="document-outline"></ion-icon></span>
+                    <label class="upload">Upload ID</label> 
+                    <input type="file" name="uploadid">  
+                   
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="document-outline"></ion-icon></span>
+                    <label class="upload">Upload Permit</label> 
+                    <input type="file" name="uploadpermit">  
+                    
+                </div>
+                <div class="remember-forgot">
+                    <label><input type="checkbox">I agree to the terms & conditions</label>
+                </div>
+                <button type="submit" class="btn">Register</button>
+                <div class="login-register">
+                    <p>Already have an account?<a href="#" class="Login-link">Login</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
+  </div>
+</div>
+
 
 
     <!-- Description -->
@@ -178,7 +279,7 @@
     </footer> 
 <!-- popup login -->
 
-<div class="wrapper" id="login-popup">
+<!-- <div class="wrapper" id="login-popup">
         <span class="icon-close"><ion-icon name="close"></ion-icon></span>
         <div class="form-box login">
             <h2>Login</h2>
@@ -205,10 +306,10 @@
                     <p><a href="#" class="register-link"></a></p>
                 </div>
             </form>
-        </div>
+        </div> -->
 
         <!-- Registration -->
-        <div class="form-box register">
+        <!-- <div class="form-box register">
             <h2>Registration</h2>
             <form action="userregister.php" method="POST">
                 <div class="input-box">
@@ -267,11 +368,27 @@
                 </div>
             </form>
         </div>
-    </div>
+    </div> -->
 </body>
 
 
+<script>
+    // JavaScript to handle the modal functionality
+document.getElementById('openModal').addEventListener('click', function() {
+  document.getElementById('modal').style.display = 'block'; // Display the modal when the button is clicked
+});
 
+document.getElementsByClassName('close')[0].addEventListener('click', function() {
+  document.getElementById('modal').style.display = 'none'; // Hide the modal when the close button is clicked
+});
+
+window.addEventListener('click', function(event) {
+  if (event.target === document.getElementById('modal')) {
+    document.getElementById('modal').style.display = 'none'; // Hide the modal if the user clicks outside of it
+  }
+});
+
+</script>
 <script src="assets/js/script.js"></script>
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
